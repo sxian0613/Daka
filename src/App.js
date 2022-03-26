6018c143f8d5e11ede7a2da14d3888036db4859c
Shiwei Xian <xweiwei@Shiweis-MacBook-Pro.local>
2022-03-25 20:35:52 -0700

    change to a game

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>杰尼龟打卡 v0.01</h1>
+      <h1>杰尼龟打卡 v0.01 （废弃⚠️）</h1>
+      <h2 class="header">得分：</h2>
+      <div class="content">
+        <div class="game">
+          <p>棋盘</p>
+        </div>
       <img class="icon" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png" alt="杰尼龟图" />
+      </div>
     </div>
   );
 }
